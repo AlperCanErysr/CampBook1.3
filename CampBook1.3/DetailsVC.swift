96f1d45e56847ca64b7f617977d482df8bd22696
Alper Can Eryasar <a.can.erysr@gmail.com>
2024-05-27 21:59:55 +0300

    save and show datails in tableview

--- CampBook1.3/DetailsVC.swift
+++ CampBook1.3/DetailsVC.swift
@@ -25,8 +25,39 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         if chosenPainting != "" {
             //CoreData
             
-            let stringUUID = chosenPaintingId!.uuidString
-            print(stringUUID)
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            fetchRequest.predicate = NSPredicate.init(format: "id = %@", idString!) //id den çağırıyoruz
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+            }catch{
+                print("error!!!")
+            }
             
         }
         else{
